b9daef421d3e46e2c75733d0e27db808b49d106e
bhagyarajmallampalli <bhagyaraj.mallampalli@gmail.com>
2018-07-03 21:46:21 +0100

    DataSource added in context.xml
    Kindly add the following DS in tomcat context.xml
    <Resource name="jdbc/SMTDS" global="jdbc/SMTDS" auth="Container" type="javax.sql.DataSource"
    maxTotal="100" maxIdle="30" maxWaitMillis="10000"
    username="root" password="Root@123" driverClassName="com.mysql.jdbc.Driver"
    url="jdbc:mysql://localhost:3306/savemytime?useSSL=true"/>
    </Context>

--- src/main/java/com/tis/savemytime/helpers/DbConnection.java
+++ src/main/java/com/tis/savemytime/helpers/DbConnection.java
@@ -1,8 +1,11 @@
 package com.tis.savemytime.helpers;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 
@@ -12,16 +15,25 @@ public class DbConnection {
 
 	private static final Logger logger = Logger.getLogger(DbConnection.class);
 	
+	
 	public Connection getConnection() {
-		try {
+		Context ctx = null;
+		Connection con = null;
+		
+		try {/*
 			String connectionURL = PropertiesHelper.getPropertyValue("jdbc_url");
 			System.out.println("jdbc url::"+connectionURL);
 			Connection connection = null;
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			String jdbcUsername = PropertiesHelper.getPropertyValue("jdbc_username");
 			String jdbcPassword = PropertiesHelper.getPropertyValue("jdbc_password");
-			connection = DriverManager.getConnection(connectionURL, jdbcUsername, jdbcPassword);
-			return connection;
+			connection = DriverManager.getConnection(connectionURL, jdbcUsername, jdbcPassword);*/
+			ctx = new InitialContext();
+			logger.debug("initial context has been set");
+			DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/SMTDS");
+			logger.debug("data source has been created");
+			con = ds.getConnection();
+			return con;
 		} catch (SQLException e) {
 			logger.error(e);
 			return null;
